5f0c827c011eb426ac8b6e4e6851180ad7e7aec4
Seu nome para exibiçãoMartarelli <renanmartarelli@gmail.com>
2022-06-08 15:45:00 -0300

    Codigo Funcionando

--- desafio-dio-banco/src/main/Conta.java
+++ desafio-dio-banco/src/main/Conta.java
@@ -28,7 +28,7 @@ public abstract class Conta implements IConta {
     protected void imprimirInfosComuns(){
         System.out.println(String.format("Agencia: %d", this.agencia));
         System.out.println(String.format("Numero: %d", this.numero));
-        System.out.println(String.format("Saldo: %d", this.saldo));
+        System.out.println(String.format("Saldo: %.2f", this.saldo));
     }
 
     public int getAgencia() {
